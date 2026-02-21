042391db5280fa90ba17cb5c63fbaa05692c063d
Vincent Wright <vmw23d@fsu.edu>
2026-02-21 08:23:42 -0500

    Fixed some things in my submit function

--- thread-pool.hpp
+++ thread-pool.hpp
@@ -1,7 +1,8 @@
 //my thread pool implementation from scratch.
 #ifndef _Vincent_threadp
-#define _Vincent_threadp
-using namespace std; 
+#define _Vincent_threadp 
+#include<type_traits>
+#include<utility>
 #include<condition_variable>
 #include<functional>
 #include<future>
@@ -11,39 +12,79 @@ using namespace std;
 #include<queue>
 class threadpool{
 	public:
-		threadpool(size_t num_threads)//my constructor
+		//constructor
+		threadpool(std::size_t num_threads)
 		{
-			for(size_t i=0;i<num_threads;i++)
+			for(std::size_t i=0;i<num_threads;i++)
 				workers.emplace_back([this]{workerLoop();}); // for number of workers, each runs the worker loop
 		}
+
+
+
+
+
+		//destructor
 		~threadpool(){
-			for(size_t i =0;i<workers.size();i++) //for every thread in the vector
-				workers[i].join(); //join them and then we can leave safely 
-			stopped = true;
+			{	std::unique_lock<std::mutex> lock(mtx); // lock mutex to stop data races again 
+				stopped = true;
+			}
+			cv.notify_all();// wake up all threads
+				 //join them and then we can leave safely 
+			for(auto& t: workers)
+				if(t.joinable()) t.join(); //every thread joins
 		}
 
-		void submit(function<void()> task){ //submit task function
-			{	lock_guard<mutex> lock(mtx); // locks our mutex to stop data races
-				tasks.push(std::move(task)); // more efficient to use move semantics, pushes task into queue
+
+
+
+
+		//submit task
+		template<typename F, typename... Args> 
+		auto submit(F&& f, Args&&...arg)-> std::future<std::invoke_result_t<F,Args...>>
+						//return a future holding whatever the callable would
+						//normally return
+		{ 
+		  	using ReturnType = std::invoke_result_t<F,Args...>;//creates type alias for return type
+								      
+			auto task = std::make_shared<std::packaged_task<ReturnType()>>(//wrapping f and args into a packaged task. 
+									     //putting that into a shared pointer so it 
+									     //can be excecuted by a worker later, 
+									     //and return a future<ReturnType>
+
+				[func = std::forward<F>(f),
+				targs = std::make_tuple(std::forward<Args>(arg)...)]() mutable{ //to allow moving them if a lamda
+					return std::apply(std::move(func),std::move(targs)); // call func with arguments inside targs
+					}
+			);
+			std::future<ReturnType> fut= task->get_future(); // get our result
+				
+			{std::lock_guard<std::mutex> lock(mtx); // locks our mutex to stop data races
+			if(stopped)
+				throw std::runtime_error("ThreadPool: Submit() called after shutdown()"); // error case
+			tasks.emplace([task=std::move(task)](){ //task is now a lambda so it can go into our queue
+				(*task)();
+				
+			});
 			}
 			cv.notify_one(); // wake up one worker to do the task we just submitted
+			return fut;// now we can return values that our workers get
 		}
 
 	private:
-		vector<thread> workers; //vector storing my worker threads
-		queue<function<void()>> tasks; //my queue that will store incoming tasks
+		std::vector<std::thread> workers; //vector storing my worker threads
+		std::queue<std::function<void()>> tasks; //my queue that will store incoming tasks
 							  //is wrapped so any callable can be stored
-		mutex mtx; //my lock to protect data
-		condition_variable cv; //The signal for work available or shutting down.
+		std::mutex mtx; //my lock to protect data
+		std::condition_variable cv; //The signal for work available or shutting down.
 					    //Lets workers sleep efficiently
 		bool stopped = false; // a flag to indicate if pool is shutting down or not.
 		void workerLoop() // A loop that runs indefinetely as pool is on.
 		{
 			for(;;) //infinite loop, stops when pool stops
 			{
-				function<void()> task; //creates variable to store callable
+				std::function<void()> task; //creates variable to store callable
 				{ //creates a block so that the lock gets destroyed when we exit
-				unique_lock<mutex>lock(mtx); //locks our mutex in
+				std::unique_lock<std::mutex>lock(mtx); //locks our mutex in
 				cv.wait(lock,[this]{return stopped || !tasks.empty();});
 					//sleep if theres no work & pool still running
 					//wake up for work or shut down to join
